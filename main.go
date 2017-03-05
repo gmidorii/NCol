ed2362bcc710371045cd89078401f2a32687ba32
tagasouichiro <stitow14@gmail.com>
2017-03-05 21:27:09 +0900

    add config file

--- main.go
+++ main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
+	"flag"
+	"github.com/BurntSushi/toml"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
 
 const urls = "urls.yaml"
+const configfile = "config.toml"
+
+var fEnv string
+var env Config
+
+type Configs struct {
+	Settings []Config
+}
+type Config struct {
+	Tag    string
+	Newsdb string
+	User   string
+	Pass   string
+}
 
 type Url struct {
 	Name string
-	Urls  []string
+	Urls []string
 }
 
 func main() {
+	flag.StringVar(&fEnv, "e", "local", "production environment")
+	flag.Parse()
+	var configs Configs
+	_, err := toml.DecodeFile(configfile, &configs)
+	if err != nil {
+		println(err)
+		return
+	}
+	for _, v := range configs.Settings {
+		if v.Tag == fEnv {
+			env = v
+			break
+		}
+	}
+	if env.Tag == "" {
+		println("not environment")
+		return
+	}
+
 	e := echo.New()
 
 	e.Use(middleware.Logger())
